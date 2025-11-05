9131f3a03addba1b22bdb650098437fe58477537
abner-stark <abnersant2005@gmail.com>
2025-11-05 11:00:54 -0300

    Update router instances and output details

--- Roteador/teste.java
+++ Roteador/teste.java
@@ -1,37 +1,37 @@
-import java.util.Scanner;//Declarar o scanner
+import java.util.Scanner;
 public class Teste {
     public static void main(String[] args){
 
-        //Instanciamento do roteador
-        Roteador rt1 = new Roteador("Tp-link", "Anvox3" ,168192361);
-        Roteador rt2 = new Roteador("Tp-shock", "Anvox4" ,168192362);
+        Roteador rt1 = new Roteador("Tp-link", "Anvox3" ,168192311);
+        Roteador rt2 = new Roteador("Multilaser", "FullPower5" ,168192322);
 
-        //Instanciamento do scanner
         Scanner sc = new Scanner(System.in);
 
-        //Ligar o roteador informado, garantindo que o outro esteja desliagdo.
 
         System.out.println("Qual roteador deseja ligar? " + "\n" +
-                "aperte 1 para escolher o 1 e 2 para escolher o 2 ....");
+                "aperte 1 para escolher o Tp-link e 2 para escolher o Multilaser ....");
         int receber = sc.nextInt();
 
         if(receber == 1){
             rt1.ligar();
             rt2.desligar();
-            System.out.println("Ligado o Roteador 1!" +
-                    "Roteador 2 desligado!");
+            System.out.println("Ligado o Roteador Tp-link!" +
+                    "Roteador Multilaser desligado!");
         }
         else{
             rt1.desligar();
             rt2.ligar();
-            System.out.println("ligado Roteador 2!" +
-                    "Roteador 1 Desligado!");
+            System.out.println("ligado Roteador Multilaser!" +
+                    "Roteador Tp-link Desligado!");
         }
 
-        //Retorna o Endereço Ip e Status
-        System.out.println("Roteador1: "+ rt1.getEnderecoIP());
-        System.out.println("Roteador1: "+ rt1.isStatus());
-        System.out.println("Roteador2: "+ rt2.getEnderecoIP());
-        System.out.println("Roteador2: "+ rt2.isStatus());
+        System.out.println("Tp-link: "+ rt1.getMarca());
+        System.out.println("Tp-link: "+ rt1.getModelo());
+        System.out.println("Tp-link: "+ rt1.getEnderecoIP());
+        System.out.println("Tp-link: "+ rt1.isStatus());
+        System.out.println("Multilaser: "+ rt2.getMarca());
+        System.out.println("Multilaser: "+ rt2.getModelo());
+        System.out.println("Multilaser: "+ rt2.getEnderecoIP());
+        System.out.println("Multilaser: "+ rt2.isStatus());
     }
 }
